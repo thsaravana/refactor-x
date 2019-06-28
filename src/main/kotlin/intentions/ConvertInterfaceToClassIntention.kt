fc7b63c146ff3933a87aae72f14f2f7f6403397f
Henry <th.saravana@gmail.com>
2019-06-28 20:56:19 +0530

    Adding Convert to Class intention

---
+++ src/main/kotlin/intentions/ConvertInterfaceToClassIntention.kt
@@ -0,0 +1,21 @@
+package intentions
+
+import com.intellij.codeInsight.intention.BaseElementAtCaretIntentionAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
+
+class ConvertInterfaceToClassIntention : BaseElementAtCaretIntentionAction() {
+
+    override fun getText() = "Convert to 'abstract class'"
+
+    override fun getFamilyName() = "Convert Interface to Class"
+
+    override fun isAvailable(project: Project, editor: Editor?, element: PsiElement): Boolean {
+        return true
+    }
+
+    override fun invoke(project: Project, editor: Editor?, element: PsiElement) {
+
+    }
+}
